1e5f4767b31cd2eba27998fd304f092853a44fb9
Patrik Haar <patrik.haar@hotmail.com>
2018-07-10 09:20:25 +0200

    Script for creating the user table

---
+++ AspNetLearning/AspNetLearning/DB/user_table.sql
@@ -0,0 +1,16 @@
+﻿IF NOT EXISTS
+		(  SELECT [name] 
+		   FROM sys.tables
+		   WHERE [name] = 'users'
+		)
+	BEGIN
+		CREATE TABLE users (
+			id INT IDENTITY(1,1) PRIMARY KEY,
+			alias VARCHAR(255) NOT NULL,
+			first_name VARCHAR(255),
+			last_name VARCHAR(255),
+			email VARCHAR(255),
+			registration_date DATETIME NOT NULL,
+			last_active DATETIME
+		);
+	END;
