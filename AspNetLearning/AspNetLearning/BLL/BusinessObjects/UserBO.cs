12bedf7fe617c651ddadc12c140f363fc7cc058f
Patrik Haar <patrik.haar@hotmail.com>
2018-07-10 15:51:21 +0200

    Added database queriying for User and UserDetailed views

--- AspNetLearning/AspNetLearning/BLL/BusinessObjects/UserBO.cs
+++ AspNetLearning/AspNetLearning/BLL/BusinessObjects/UserBO.cs
@@ -8,10 +8,24 @@ namespace AspNetLearning.BLL
 {
     public class UserBO
     {
+        public int Id { get; set; }
         public string Alias { get; set;}
         public string FirstName { get; set; }
         public string LastName { get; set;}
         public DateTime RegistrationDate { get; set; }
 
+        public UserBO()
+        {
+            
+        }
+
+        public UserBO(int id, string alias, string firstName, string lastName, DateTime registrationDate)
+        {
+            this.Id = id;
+            this.Alias = alias;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.RegistrationDate = registrationDate;
+        }
     }
 }
